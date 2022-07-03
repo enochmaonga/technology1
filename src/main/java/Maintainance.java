8e32a09deb387e0347921f786273290810fdcc70
enochmaonga <enoch.mokua@student.moringaschool.com>
2022-07-03 12:06:27 +0300

    added names of staff to the list

--- src/main/java/Maintainance.java
+++ src/main/java/Maintainance.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 
 public class Maintainance {
     public static void  Main(String[]args){
-        System.out.println("We are coming");
+        String staff_names ="Jane Ombati,James Bold,Alfred Karani, Francis Keen," +
+                "Bernard Kemboi, Kennedy Kenj, Edna White, Raphael Wheles, Joyce Mungai, " +
+                "Adriel Isensi, Ombuki James, Kamau Njau, Komen Joseph, Rambui John, " +
+                "Alleine Joy, " +
+                "Masmau Joyce, Albois Wayne, Joseph Kombo, Superman Omondi, Edda Boyne";
+
+
+        }
     }
-}
+
