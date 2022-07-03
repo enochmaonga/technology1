6e3fcaf1f46c1dfd57657b2097860e416c700f3f
Administrator <enoch.maonga@gmail.com>
2022-07-03 14:12:51 +0300

    new string cybersecurity

--- src/main/java/Maintainance.java
+++ src/main/java/Maintainance.java
@@ -1,7 +1,6 @@
-import java.util.Scanner;
-
-
 public class Maintainance {
+    private static String cybersecurity;
+
     public static void  Main(String[]args){
         String staff_names ="Jane Ombati,James Bold,Alfred Karani, Francis Keen," +
                 "Bernard Kemboi, Kennedy Kenj, Edna White, Raphael Wheles, Joyce Mungai, " +
@@ -9,7 +8,9 @@ public class Maintainance {
                 "Alleine Joy, " +
                 "Masmau Joyce, Albois Wayne, Joseph Kombo, Superman Omondi, Edda Boyne";
 
-
+        String name = new String("Cybersecurity");
+        cybersecurity= "Jane Ombati,Alfred Karani,Francis Keen, Bernard Kemboi";
+        System.out.println("name");
         }
     }
 
---
+++ src/main/java/Models/Post.java
@@ -0,0 +1,21 @@
+//import org.junit.jupiter.api.extension.ExtendWith;
+//
+//@ExtendWith(ExtensionVogellaLifeCycle.class)
+//class LifeCycleExtensionTest {
+//
+//    @Test
+//    void test1() {
+//        assertTrue(true);
+//    }
+//
+//    @Test
+//    void test2() {
+//        assertTrue(true);
+//    }
+//
+//    @Test
+//    void test3() {
+//        assertTrue(true);
+//    }
+//
+//}
